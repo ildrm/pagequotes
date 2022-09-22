086f53118bffd1de5d14cae2a38820bdd2becc35
Shahin Ilderemi <ildrm@hotmail.com>
2022-09-23 01:07:14 +0330

    first commit

---
+++ src/PageQuotes.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Class Quote
+ */
+class PageQuotes {
+    private $quotes;
+
+    /**
+     * @param array $quotes
+     */
+    function __construct(array $quotes=array())
+    {
+        if (count($quotes))
+        {
+            $this->init($quotes);
+            $this->quotes($quotes);
+        }
+    }
+
+    static function quotes(array $quotes=array()) {
+
+    }
+
+    /**
+     * @param array $quotes
+     * @return void
+     */
+    public function init(array $quotes)
+    {
+        if (isset($quotes['quotes']) and !empty($quotes['quotes'])) $this->quotes = $quotes['quotes'];
+    }
+
+    /**
+     * @param string $page
+     * @param int $max
+     * @return array
+     */
+    public function getQuotes(string $page, int $max=0): array
+    {
+        $quotes = array();
+        if ($max) {
+            $quotes = array_slice($this->quotes[$page],$max);
+        } else {
+            $quotes = $this->quotes[$page];
+        }
+        return $quotes;
+    }
+
+    /**
+     * @param string $page
+     * @param int $max
+     * @return string
+     */
+    public function printQuotes(string $page, int $max=0): string
+    {
+        $quotes = $this->getQuotes($page, $max);
+        $html = '';
+        foreach ($quotes as $quote) {
+            $html .= "<div>$quote</div>\n";
+        }
+        return $html;
+    }
+}
